e1bcfdf53b23672bbb4c3ca7f193e1ba834b7a66
Andrew Duerig <gamechefproductions@gmail.com>
2023-05-26 14:50:30 -0400

    day16

--- 16.py
+++ 16.py
@@ -5,12 +5,14 @@ from helpers import *
 from collections import deque
 import re
 import pathlib
+import time
 
 filepath = pathlib.Path(__file__)
 
 data_file = filepath.parent.joinpath(filepath.stem + '.dat')
 
 
+start_time = time.time()
 
 graph = {}
 flow_rates = {}
@@ -49,11 +51,13 @@ for lol, ok in graph.items():
     print(lol, ok)
 
 
+
+
 all_distances = {}
 def bfs_all(orig):
-    queue = [(orig, 0, set())]
+    queue = deque([(orig, 0, set())])
     while queue:
-        the_tuple = queue.pop(0)
+        the_tuple = queue.popleft()
         node, depth, the_set = the_tuple
         
         if node in the_set:
@@ -74,7 +78,9 @@ for valve in graph:
     bfs_all(valve)
 
 
-distances_without_zero = {}
+distances_without_zero = {
+    'DONE': {},
+}
 for node, distances in all_distances.items():
     if node in zero_fellas and node != 'AA':
         continue
@@ -85,59 +91,88 @@ for node, distances in all_distances.items():
 
 for node, distances in distances_without_zero.items():
     print(green(node), distances)
-# exit()
+print_yellow(f'bfs took {time.time() - start_time} seconds')
 
-
-queue = deque([('AA', 'AA', 0, 0, 0, 26, 26, set())])
+queue = deque([(False, 'AA', 'AA', 0, 0, 0, 26, 26, set())])
 # path_finished = []
 max_pres = 0
+steps = 0
+start_time_2 = time.time()
 while queue:
-    print(len(queue))
-    the_tuple = queue.popleft()
-    valve_1, valve_2, total_pressure, rate_1, rate_2, minutes_left_1, minutes_left_2, visited = the_tuple
+    steps += 1
+    the_tuple = queue.pop()
+    break_later, valve_1, valve_2, total_pressure, rate_1, rate_2, minutes_left_1, minutes_left_2, visited = the_tuple
 
-    # print(valve, path, minutes_left, visited, len(queue))
-    if valve_1 in visited or valve_2 in visited:
+    if random.randint(1, 100000) == 2:
+        print(f'{len(queue)=}, {max_pres=}, {steps=:,.0f}, {valve_1=}, {valve_2=} {steps / (time.time() - start_time_2)=:,.0f}')
+
+
+    # if break_later or (vb
+    if (valve_1 != 'DONE' and valve_1 in visited) or (valve_2 != 'DONE' and valve_2 in visited):
         continue
-    new_visited = visited.union(set([valve_1, valve_2]))
+    visited.add(valve_1), 
+    visited.add(valve_2)
     if minutes_left_1 <= 0 and minutes_left_2 <= 0:
         continue
-    # if path:
-    #     path_finished.append(list(path))
     
-    max_pres = max(max_pres, total_pressure + (rate_1 * max(minutes_left_1, 0)) + (rate_2 * max(minutes_left_2, 0)))
+    leave_till_end = total_pressure + (rate_1 * minutes_left_1) + (rate_2 * minutes_left_2)
+    max_pres = max(max_pres, leave_till_end)
 
+    possi_1 = []
     for index1, next_valve_1 in enumerate(distances_without_zero[valve_1]):
-        for index2, next_valve_2 in enumerate(distances_without_zero[valve_2]):
-            # if index2 < index1:
-            #     continue
-            if index1 == index2:
+        cost_1 = distances_without_zero[valve_1][next_valve_1]
+
+        if cost_1 + 1 <= minutes_left_1:
+            possi_1.append((
+                next_valve_1, 
+                (cost_1 + 1) * rate_1, 
+                rate_1 + flow_rates[next_valve_1], 
+                minutes_left_1 - (cost_1 + 1))
+            )
+    possi_1.append((
+        'DONE', 
+        minutes_left_1 * rate_1, 
+        rate_1, 
+        0,
+    ))
+
+    possi_2 = []
+    for index2, next_valve_2 in enumerate(distances_without_zero[valve_2]):
+        cost_2 = distances_without_zero[valve_2][next_valve_2]
+
+        if cost_2 + 1 <= minutes_left_2:
+            possi_2.append((
+                next_valve_2, 
+                (cost_2 + 1) * rate_2, 
+                rate_2 + flow_rates[next_valve_2], 
+                minutes_left_2 - (cost_2 + 1))
+            )
+    possi_2.append((
+        'DONE', 
+        minutes_left_2 * rate_2, 
+        rate_2, 
+        0,
+    ))
+
+    # print(possi_1)
+    for next_valve_1, pressure_added_1, temp_rate_1, temp_minutes_left_1 in possi_1:
+        for next_valve_2, pressure_added_2, temp_rate_2, temp_minutes_left_2 in possi_2:
+            if (next_valve_1 == next_valve_2) and next_valve_1 != 'DONE':
                 continue
-            
-            cost_1 = distances_without_zero[valve_1][next_valve_1]
-            cost_2 = distances_without_zero[valve_2][next_valve_2]
-            blah = total_pressure
+                
+            new_total_pressure = total_pressure + pressure_added_1 + pressure_added_2
+            max_pres = max(max_pres, new_total_pressure)
+            if next_valve_1 == next_valve_2:
+                continue
 
-            temp_rate_1 = rate_1
-            if cost_1 + 1 <= minutes_left_1:
-                blah += ((cost_1 + 1) * temp_rate_1)
-                temp_rate_1 += flow_rates[next_valve_1]
+            queue.append((break_later, next_valve_1, next_valve_2, new_total_pressure, temp_rate_1, temp_rate_2, temp_minutes_left_1, temp_minutes_left_2, set(visited)))
 
-            temp_rate_2 = rate_2
-            if cost_2 + 1 <= minutes_left_2:
-                blah += ((cost_2 + 1) * temp_rate_2)
-                temp_rate_2 += flow_rates[next_valve_2]
-
-
-            print(f'{minutes_left_1=}, {cost_1=}, {minutes_left_2=}, {cost_2=}, {max_pres=}, {temp_rate_1=}, {temp_rate_2=}, {blah=}')
-            if cost_1 + 1 <= minutes_left_1 or cost_2 + 1 <= minutes_left_2:
-                queue.append((next_valve_1, next_valve_2, blah, temp_rate_1, temp_rate_2, minutes_left_1 - (cost_1 + 1), minutes_left_2 - (cost_2 + 1), new_visited))
-            
-            # if next_valve_2 == 'DD' and next_valve_1 == 'JJ':
-            #     exit()
-
+print(steps)
 print_green(f'{max_pres=}')
+print_yellow(f'{time.time() - start_time} total seconds')
 
+# 1971 is too low
+# 1991 is too low
 
 
 # # https://adventofcode.com/2022
