5fe88a104ce45449207fbb58162c422345a33a1a
Andrew Duerig <gamechefproductions@gmail.com>
2023-06-05 16:58:40 -0400

    pt 2

--- 24.py
+++ 24.py
@@ -9,7 +9,10 @@ filepath = pathlib.Path(__file__)
 data_file = filepath.parent.joinpath(filepath.stem + '.dat')
 
 
+grid = []
 with open(data_file) as f:
     for line in f.readlines():
         line = line.strip()
+        if line:
+            list(line)
 
